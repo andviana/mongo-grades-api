1ba399b5a9db86279ef0abe94d2ba916851c23d4
Anderson Viana <andiana@hotmnai.com>
2020-07-10 22:03:31 -0300

    commit inicial

---
+++ models/studentModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export default (mongoose) => {
+    const studentSchema = mongoose.Schema({
+        name: { type: String, required: true },
+        subject: { type: String, required: true },
+        type: { type: String, required: true },
+        value: {
+            type: Number,
+            require: true,
+            validate(value) {
+                if (value < 0) throw new Error('Valor negativo para a nota não permitido');
+            },
+        },
+        lastModified: { type: Date, default: Date.now },
+    });
+
+    const Student = mongoose.model('student', studentSchema, 'students');
+
+    return Student;
+}
+
