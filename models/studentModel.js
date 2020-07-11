6ac64f3cdbe0a7325f9fc2b09a0b75fcc67d9921
Anderson Viana <andiana@hotmnai.com>
2020-07-11 00:31:15 -0300

    ajuste deserialização adiicionando campo ID

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,22 +1,28 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export default (mongoose) => {
-    const studentSchema = mongoose.Schema({
-        name: { type: String, required: true },
-        subject: { type: String, required: true },
-        type: { type: String, required: true },
-        value: {
-            type: Number,
-            require: true,
-            validate(value) {
-                if (value < 0) throw new Error('Valor negativo para a nota não permitido');
-            },
-        },
-        lastModified: { type: Date, default: Date.now },
-    });
+  const studentSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    subject: { type: String, required: true },
+    type: { type: String, required: true },
+    value: {
+      type: Number,
+      require: true,
+      validate(value) {
+        if (value < 0)
+          throw new Error('Valor negativo para a nota não permitido');
+      },
+    },
+    lastModified: { type: Date, default: Date.now },
+  });
 
-    const Student = mongoose.model('student', studentSchema, 'students');
+  studentSchema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject();
+    /*remove(object.password);*/
+    object.id = _id;
+    return object;
+  });
+  const Student = mongoose.model('student', studentSchema, 'students');
 
-    return Student;
-}
-
+  return Student;
+};
